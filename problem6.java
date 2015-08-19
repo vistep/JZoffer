9dc5081d8e02278541639e3e4b4275ac2e01d059
vistep <shujiaming@hotmail.com>
2015-08-19 09:51:33 +0800

    full version of problem6

--- problem6.java
+++ problem6.java
@@ -6,10 +6,12 @@ public class problem6 {
 		if (T == null) {
 			return;
 		} else {
-
+//			System.out.print(T.val);
 			search(T.left);
+//			System.out.print(T.val);
 			search(T.right);
 			System.out.print(T.val);
+
 
 		}
 	}
@@ -19,17 +21,44 @@ public class problem6 {
 		node p = T;
 		while(p != null || !stack.isEmpty()) {
 			if (p != null) {
-				System.out.print(p.val);
+//				System.out.print(p.val);
 				stack.add(p);
 				p = p.left;
 			} else {
-				
-				
+				p = stack.get(stack.size() - 1);
+				System.out.print(p.val);				
+				stack.remove(stack.size() -1);
+				p = p.right;				
+			}
+		}
+	}
+	
+	public static void search2(node T) {
+		if (T == null) {
+			System.err.println("Empty tree!");
+			return;
+		}
+		List<node> stack = new ArrayList<node>();
+		node cur;
+		node pre = null;
+		stack.add(T);
+		while (!stack.isEmpty()) {
+			cur = stack.get(stack.size() - 1);
+			if ((cur.left == null && cur.right == null) || 
+					(pre != null && (cur.left == pre || cur.right == pre))) {
+				System.out.print(cur.val);
+				pre = cur;
+				stack.remove(stack.size() - 1);
+			} else {
+				if (cur.right != null) {
+					stack.add(cur.right);
+				}
+				if (cur.left != null) {
+					stack.add(cur.left);
+				}
 			}
 
 		}
-		
-
 	}
 
 	public static void main(String[] args) {
@@ -41,7 +70,47 @@ public class problem6 {
 		search(T);
 		System.out.println();
 		search1(T);
+		System.out.println();
+		search2(T);
 
 	}
 
 }
+
+class node {
+	int val;
+	node left;
+	node right;
+}
+
+class buildTree {
+	public node construct(int[] preorder, int[] medorder, int length) {
+		if (preorder == null || medorder == null || length < 1) {
+			return null;
+		} else {
+			return constructCore(preorder, medorder, 0, length - 1, 0, length - 1);
+		}
+	}
+
+	public node constructCore(int[] preorder, int[] medorder, int pbegin,
+			int pend, int mbegin, int mend) {
+		if (pbegin > pend || mbegin > mend) {
+			return null;
+		} else {
+			node cur = new node();
+			cur.val = preorder[pbegin];
+			int i;
+			for (i = mbegin; i <= mend; i++) {
+				if (medorder[i] == preorder[pbegin]) {
+					break;
+				}
+			}
+			cur.left = constructCore(preorder, medorder, pbegin + 1, pbegin + i
+					- mbegin, mbegin, i - 1);
+			cur.right = constructCore(preorder, medorder, pbegin + i - mbegin
+					+ 1, pend, i + 1, mend);
+			return cur;
+		}
+	}
+}
+
